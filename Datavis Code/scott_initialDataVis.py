f2b831730a77cd9ee541af71dc6200dccd43a94d
scmiller005 <102572364+scmiller005@users.noreply.github.com>
2025-03-12 23:17:01 -0700

    Beginning of organizing repo

--- Datavis Code/scott_initialDataVis.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib as plt
-
-pranav_AADT_2017_2022 = pd.read_csv('C:/Users/zyplo/Documents/GitHub/ECE 143/ECE143_Group8/Data/pranav_cleaned_data/AADT/combined_aadt_2017_2022.csv')
-print(pranav_AADT_2017_2022.head())
-print(pranav_AADT_2017_2022.describe())
-print(pranav_AADT_2017_2022.value_counts())
